1dd3bdd269cfac596f7644d75c120765ded93a03
Rui Chen <chenruix47@gmail.com>
2022-08-07 15:57:41 -0400

    changed setting to use env variable

--- capstone/settings.py
+++ capstone/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.environ.get('SECRET_KEY','didnotgetkeyvalue')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG',False))
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
