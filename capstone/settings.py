a2ab4432ba48eb8455ff1ade5aaaea6242a24563
Rui Chen <chenruix47@gmail.com>
2022-08-07 23:03:13 -0400

    added csrf_trusted_origin

--- capstone/settings.py
+++ capstone/settings.py
@@ -28,6 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY','didnotgetkeyvalue')
 DEBUG = bool(os.environ.get('DEBUG',False))
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+if(os.environ.get('DEVELOPEMENT')):
+    CSRF_TRUSTED_ORIGINS = []
+else:
+    CSRF_TRUSTED_ORIGINS=['https://agile-waters-93639.herokuapp.com/']
 
 
 # Application definition
