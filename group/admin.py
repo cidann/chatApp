31caaeec565aa0d0b288e4dbecd505e993187926
Rui Chen <chenruix47@gmail.com>
2022-08-10 21:31:24 -0400

    added models to admin

--- group/admin.py
+++ group/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from group.models import User,Message,Group,Membership
 # Register your models here.
+class DefaultAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(User,DefaultAdmin)
+admin.site.register(Message,DefaultAdmin)
+admin.site.register(Group,DefaultAdmin)
+admin.site.register(Membership,DefaultAdmin)
